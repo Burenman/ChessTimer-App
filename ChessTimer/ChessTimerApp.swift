2570be5cf6a997a8577e43e23d57bfdcc2ab82e4
Viktor <Burenin777@gmail.com>
2023-11-23 10:27:15 -0500

    Initial Commit

---
+++ ChessTimer/ChessTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessTimerApp.swift
+//  ChessTimer
+//
+//  Created by Viktor on 2023-11-23.
+//
+
+import SwiftUI
+
+@main
+struct ChessTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
